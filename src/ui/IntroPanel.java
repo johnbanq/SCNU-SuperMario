38528357360757ffab3b66cf94f6cab932fcf5ac
johnbanq <johnbanq@gmail.com>
2018-11-04 23:11:57 +0800

    animation of intro 2

--- src/ui/IntroPanel.java
+++ src/ui/IntroPanel.java
@@ -15,7 +15,7 @@ import ui.events.GameStartEvent;
 
 @SuppressWarnings("serial")
 public class IntroPanel extends AbstractGamePanel {
-	
+
 	public IntroPanel(SoundManager sound_mgr) {
 		super(sound_mgr);
 	}
@@ -24,18 +24,15 @@ public class IntroPanel extends AbstractGamePanel {
 	private JLabel intro_label;
 	private BlinkingLabel guide_label;
 	private KeyListener listener;
-	
-	private final String intro_text = "<html><body>Zero娘是Zero动漫社的宝贝<br>但在校庆当天，意外发生了——<br>Zero娘被天空中的娃娃机抓手抓到了一课里，<br>赶快去一课救她吧！</body></html>";
+
+	private final String[] intro_text = { "Zero娘是Zero动漫社的宝贝", "但在校庆当天，意外发生了——", "Zero娘被天空中的娃娃机抓手抓到了一课里，", "赶快去一课救她吧！" };
 	private final String guide_text = "<html><body>按任意键开始游戏</body></html>";
-	
-	//render info
-	private boolean should_add_alpha = false;
-	private int alpha = 1;
-	
+
+	// render info
 	private int tick_to_skip = 0;
-	private int shown_ch_count = 0;
-	private StringBuffer shown_buf = new StringBuffer();
-	
+	private int shown_row_count = 0;
+	private StringBuffer shown_buf = new StringBuffer("<html><body>");
+
 	@Override
 	public void removeFromGameClient(GameClient gc) {
 		gc.removeKeyListener(listener);
@@ -46,75 +43,70 @@ public class IntroPanel extends AbstractGamePanel {
 	@Override
 	public void addToGameClient(GameClient gc) {
 		bg_label = new JLabel();
-		bg_label.setBounds(-5,-26,800,600);
+		bg_label.setBounds(-5, -26, 800, 600);
 		bg_label.setVisible(true);
 		bg_label.setOpaque(true);
 		bg_label.setBackground(Color.BLACK);
 		gc.getContentPane().add(bg_label);
-		
+
 		intro_label = new JLabel();
-		intro_label.setText(intro_text);
 		intro_label.setForeground(Color.WHITE);
 		intro_label.setFont(new Font("微软雅黑", 1, 21));
-		intro_label.setBounds(20,0,500,200);
-		intro_label.setOpaque(false);  
+		intro_label.setBounds(20, 0, 500, 200);
+		intro_label.setOpaque(false);
 		bg_label.add(intro_label);
-		
+
 		guide_label = new BlinkingLabel();
 		guide_label.setText(guide_text);
 		guide_label.setForeground(Color.WHITE);
 		guide_label.setFont(new Font("微软雅黑", 1, 15));
-		guide_label.setBounds(600,500,200,50);
+		guide_label.setBounds(600, 500, 200, 50);
 		guide_label.setOpaque(false);
 		guide_label.setVisible(false);
 		bg_label.add(guide_label);
-		
-		//the press any key listener
-		if(listener==null) {
+
+		// the press any key listener
+		if (listener == null) {
 			listener = new KeyAdapter() {
 				@Override
 				public void keyPressed(KeyEvent e) {
 					tellEvent(new GameStartEvent());
-				}	
+				}
 			};
 		}
 		bg_label.addKeyListener(listener);
 		bg_label.requestFocus();
 	}
-	
+
 	public synchronized void paint(Graphics g) {
-		//HACK:to make scrolling effect and lighting effect
-		//frame rate assumed to be 50Hz according to framerate in PaintThread
+		// HACK:to make scrolling effect and lighting effect
+		// frame rate assumed to be 50Hz according to framerate in PaintThread
+
+		// make the intro type
+		tick_intro_text();
 		
-		//make the intro type
-		if(tick_to_skip!=0) {
+		bg_label.paint(g);
+	}
+
+	private void tick_intro_text() {
+		// make the intro type
+		if (tick_to_skip != 0) {
 			tick_to_skip--;
-		}else {
-			//header
-			if(shown_ch_count == 0) {
-				shown_buf.append("<html><body>");
-			}
-		
-			if(shown_ch_count<0) {
-				//Noop: means finished display
-			}else if(shown_ch_count<intro_text.length()) {
-				char ch = intro_text.charAt(shown_ch_count);
-				if(ch=='\n') {
-					shown_buf.append("<br>");
-				}else {
-					shown_buf.append(ch);
-				}
-				shown_ch_count++;
-				//display delay
-				tick_to_skip=1;
-			}else {//fully displayed
-				guide_label.setVisible(true);
-				shown_ch_count=-1;
-			}
-			intro_label.setText(shown_buf.toString()+"</html></body>");
+			return;
 		}
-        
-        bg_label.paint(g);
+
+		if (shown_row_count < 0) {
+			// Noop: means finished display
+		} else if (shown_row_count < intro_text.length) {
+			shown_buf.append(intro_text[shown_row_count++]);
+			shown_buf.append("<br>");
+			// display delay
+			tick_to_skip = 40;
+		} else {// shown_row_count == intro_text.length fully displayed
+			guide_label.setVisible(true);
+			shown_row_count = -1;
+		}
+		intro_label.setText(shown_buf.toString() + "</html></body>");
 	}
 
 }
